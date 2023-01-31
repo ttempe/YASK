51bc086f2ee2e705c371c5743e08d411faa30195
Thomas TEMPÉ <thomas.tempe@alysse.org>
2023-01-31 09:22:54 +0800

    HW version V1.1
    Change some pin assignments to make them compatible with Raspberry Pi RP Pico original boards.

--- YASK.py
+++ YASK.py
@@ -4,6 +4,8 @@ from machine import Pin
 
 #Config:
 FirstUpChordSend = True
+AutoRepeat = False
+#TODO: add auto-repete on a specified list of strokes (or values + masks) (or any, on second press?)
 
 ### Extract from Plover code:
 # In the Gemini PR protocol, each packet consists of exactly six bytes
@@ -34,8 +36,9 @@ FirstUpChordSend = True
 #               ["-P", "-B", "-L", "-G", "-T", "-S", "-D"],
 #               ["Num", "Num", "Num", "Num", "Num", "Num", "-Z"]]
 
+
 #List of GPIOs connected to chording keys
-__inputs =     [0, 1,  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 19, 20, 21, 22, 26, 27, 28, 29, 23, 17] #GPIO address
+__inputs =     [0, 1,  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 19, 20, 21, 22, 26, 27, 28, 15, 14, 17] #GPIO address
 #Corresponds to:S- T-  K- P- W- H- R- A  O  *  #    E   U  -F  -R  -P  -B  -L  -G  -T  -S  -D  -Z, N/A #key name
 #Index          0  1   2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  #list index
 
@@ -49,11 +52,22 @@ __protocole = [[ 23, 10, 10, 10, 10, 10, 10],
 
 #Bitmaps
 #                ~ZDSTGLBPRFUE#*OARHWPKTS
-__left_hand =  0b000000000000000111111111
+__left_hand  = 0b000000000000000111111111
 __right_hand = 0b011111111111100000000000
+
+# Autorepeat configuration: 
+#                      key states                  mask: which keys to match?
+#                        ZDSTGLBPRFUE#*OARHWPKTS    ZDSTGLBPRFUE#*OARHWPKTS
+__autoRepeatRules = [ (0b00000000000000001010101, 0b11110000001111111111111),  #Ted's navigation: move cursor
+                      (0b00000000000000001010101, 0b11110000001111111111111),  #Ted's navigation: move cursor
+                      (0b00000000000000000101011, 0b11110000001111111111111),  #Ted's navigation: select
+                    ]
+__autoRepeatDelay = 700 #before 1st repeat
+__autoRepeatRepeat= 300 #between subsequent repeats
+
 __LED = Pin(25, Pin.OUT)
 
-class YACK:    
+class YASK:    
     def __init__(self):
         self.keymap = {}
         self.inputs_max = 0 #Bitmap (order is same as __inputs) 
@@ -76,8 +90,14 @@ class YACK:
         left, right, left_max, right_max = (0, 0, 0, 0) #bitmaps, like self.input_max
         already_written = False
         timeStamp = 0
-        
+        inputs = 0
+        t0 = 0
+        repeatingStarted = False
+        autoRepeatEngaged = False
+       
         while True:
+            #polls in a loop
+            inputs_old = inputs
             inputs = 0
             for i, v in enumerate(self.keys):
                 inputs += (v()^1) << i
@@ -87,27 +107,46 @@ class YACK:
             left_max  = left  | left_max
             right_max = right | right_max
 
-            if self.inputs_max >0: #pressed
-                if inputs == 0:
-                    if already_written:
-                        already_written = False
-                    else:
+            if inputs_old != inputs:
+                #A change in the keypresses
+                t0 = time.ticks_ms()
+                repeatingStarted = False
+                autoRepeatEngaged = False
+                if self.inputs_max >0: #pressed
+                    if inputs == 0:
+                        if already_written:
+                            already_written = False
+                        else:
+                            __LED(1)
+                            #print(bin(self.inputs_max))
+                            self.write()
+                        self.inputs_max = 0
+                        left_max, right_max = (0, 0)
+                    elif (( left == 0 and left_max > 0 and already_written != "R") or (right == 0 and right_max > 0 and already_written != "L")) and FirstUpChordSend: #only released the left or right hand
                         __LED(1)
                         self.write()
-                    self.inputs_max = 0
-                    left_max, right_max = (0, 0)
-                elif (( left == 0 and left_max > 0) or (right == 0 and right_max > 0)) and FirstUpChordSend: #only released the left or right hand
-                    __LED(1)
+                        self.inputs_max = inputs
+                        if left == 0:
+                            left_max = 0
+                            already_written = "L" if right else True
+                        if right == 0:
+                            right_max= 0
+                            already_written = "R" if left else True
+                if AutoRepeat and inputs != 0 and self.inputs_max == inputs:
+                    for keys, mask in __autoRepeatRules:
+                        if (inputs & mask) == (keys & mask): #satisfy at least one rule
+                            autoRepeatEngaged = True
+                            __LED(1);time.sleep_ms(100)
+            elif autoRepeatEngaged:
+                if repeatingStarted == False and time.ticks_ms()-t0 > __autoRepeatDelay:
                     self.write()
-                    self.inputs_max = inputs
-                    if left == 0:
-                        left_max = 0
-                    if right == 0:
-                        right_max= 0
-                    already_written = True #From now on, only repeat left or right hand releases will be registered, until all keys are released
-                
-            time.sleep_ms(5)
+                    repeating = True
+                    t0 = time.ticks_ms()
+                elif repeatingStarted == True and time.ticks_ms()-t0 > __autoRepeatRepeat:
+                    self.write()
+                    t0 = time.ticks_ms()
+            time.sleep_ms(20)#anti-rebound
             __LED(0)
 
-c=YACK()
+c=YASK()
 c.loop()
